b4a8de04431d5a0e9644a308192829ea3ab8c064
Thomas Hegeman <hegemant001@gmail.com>
2022-12-05 13:59:23 -0500

    finished 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,14 @@
-const findTheOldest = function() {
+// Looked up, since I was confused about accumulator being null at the start
+// or undefined, though I was close enough.
+const findTheOldest = function (people) {
+
+  return people.reduce((acc, curr) => {
+    const accAge = acc.yearOfDeath - acc.yearOfBirth;
+    const currAge = curr.yearOfDeath - curr.yearOfBirth;
+
+    return accAge > currAge ? acc : curr;
+
+  });
 
 };
 
